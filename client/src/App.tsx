21792353f994313ac8f29a0c7a9d7b20b8b97b6e
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-10-23 19:18:46 +0300

    feat: initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { QueryBuilder, formatQuery, RuleGroupType } from "react-querybuilder";
+import "react-querybuilder/dist/query-builder.css";
+
+function App() {
+  const [query, setQuery] = useState<RuleGroupType>({
+    id: "root",
+    combinator: "and",
+    rules: [],
+  });
+
+  return (
+    <>
+      <QueryBuilder
+        fields={[{ name: "test", label: "Test" }]}
+        query={query}
+        onQueryChange={(q) => setQuery(q)}
+      />
+      <pre>{formatQuery(query, "sql")}</pre>
+      <pre>{formatQuery(query, "json")}</pre>
+    </>
+  );
+}
+
+export default App;
